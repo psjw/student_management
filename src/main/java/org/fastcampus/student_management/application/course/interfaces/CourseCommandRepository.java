4365f10269cde85710b3a711c2bfda1f7438ca40
psjw <45330194+psjw@users.noreply.github.com>
2025-03-20 21:27:15 +0900

    Delete src/main/java/org/fastcampus/student_management/application/course/interfaces directory

--- src/main/java/org/fastcampus/student_management/application/course/interfaces/CourseCommandRepository.java
+++
@@ -1,8 +0,0 @@
-package org.fastcampus.student_management.application.course.interfaces;
-
-import org.fastcampus.student_management.domain.Course;
-
-public interface CourseCommandRepository {
-    void save(Course course);
-
-}
